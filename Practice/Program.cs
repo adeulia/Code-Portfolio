6780a48fac332bfcc1b40d5eb37d1af456103945
Adrian <105984281+adeulia@users.noreply.github.com>
2022-06-29 18:40:46 -0500

    Update Program.cs

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -10,10 +10,7 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            WeatherUtilites.Report("Houston", 37, 15);
-
             FizzBuzz();
-
         }
 
         void Argentina()
@@ -51,33 +48,6 @@ namespace Practice
             {
                 return $"({X},{Y},{Z})";
             }
-        }
-    }
-    
-    public class WeatherUtilites
-    {
-        static float FahrenheitToCelsius(float tempFahrenheit)
-        {
-            float tempCelsius = tempFahrenheit - 32 / 1.8f;
-            return tempCelsius;
-        }
-
-        static float CelsiusToFahrenheit(float tempCelsius)
-        {
-            float tempFahrenheit = tempCelsius * 1.8f + 32;
-            return tempFahrenheit;
-        }
-
-        static float ComfortIndex(float tempFahrenheit, float humidityPercent)
-        {
-            float comfortIndex = (tempFahrenheit + humidityPercent) / 4;
-            return comfortIndex;
-        }
-
-        public static void Report(string location, float tempCelsius, float humidity)
-        {
-            Console.WriteLine("Comfort Index for " + location + ": " + ComfortIndex(CelsiusToFahrenheit(tempCelsius), humidity));
-            Console.ReadLine();
         }
     }
 }
