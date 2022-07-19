f6e6cbbb4f811f80aabf58fc43bd14dfe94321d5
Adrian <105984281+adeulia@users.noreply.github.com>
2022-07-19 16:25:44 -0500

    Update Person.cs

--- Practice/Person.cs
+++ Practice/Person.cs
@@ -8,10 +8,6 @@ namespace Practice
        //properties with auto implemneted getters/setters
         public string fullName { get; set; }
         public int Age { get; set; }
-        public int AppUserId { get; get; }
-        public string firstName { get; set; }
-        public string lastName { get;set; }
-        public string address { get; set; }
-  
+        public int AppUserId { get; get; }  
     }
 }
